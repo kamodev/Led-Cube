4609ce5858cfcfe00a3156315e49a5eeb0c9f7b7
unknown <jbagula@jbagula-7.corp.auctiva.com>
2013-11-27 10:17:38 -0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 Led-Cube
 ========
 
-3x3x3 LED Cube
+3x3x3 LED Cube project for fun and designing how it works with the Arduino.
+
+####Part List
+|Part Manufacturer|Distributor|Part Number|Price Per Unit|Quantity|
+
---
+++ _3x3x3_Led_Cube/_3x3x3_Led_Cube.ino
@@ -0,0 +1,84 @@
+
+int mode = 0;
+
+// Setup the device for use
+void setup()
+{
+  // Set the ports  
+  PORTB = B11111111;
+  PORTC = B00000000;
+  PORTD = B10100101;
+  
+  // Attach Interrupts
+  attachInterrupt(0, intrNLight, RISING);    // Add night light switch
+  attachInterrupt(1, intrChangeMode, FALLING;    // Add change mode button
+  attachInterrupt(2, intrRndMode, FALLING);    // Add random mode button  
+}
+
+// Main program
+void loop()
+{
+   
+}
+
+// All the Interrupt handlers
+// Night light mode
+void intrNLight()
+{
+//  TODO: Add code for this mode
+}
+
+// Random pattern mode
+void intrRndMode()
+{
+  mode = 9;
+}
+
+// Change mode of the cube
+void intrChangeMode()
+{
+  if (mode == 8)
+  {
+    mode = 0;
+  }
+  else
+  {
+    mode++;
+  }
+}
+
+// All of the different modes
+void doMode()
+{
+  switch(mode)
+  {
+    case 0:
+      // TODO: Mode 
+      break;
+
+    case 1:
+      // TODO: Mode 
+      break;
+    
+    case 2:
+      // TODO: Mode 
+      break;
+    
+    case 3:
+      // TODO: Mode 
+      break;
+      
+    case 4:
+      // TODO: Mode 
+      break;
+      
+    case 5:
+      // TODO: Mode 
+      break;
+    
+    case 9:
+      // TODO: Mode 
+      break;
+  }
+}
+
